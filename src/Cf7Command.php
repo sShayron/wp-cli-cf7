46aaa5c5abb8dceef690476d8a4188fa51d668af
Shayron Aguiar <shayron.aguiar@resultate.com.br>
2017-10-05 15:27:02 -0300

    wp-cli.yml

--- src/Cf7Command.php
+++ src/Cf7Command.php
@@ -1,11 +1,10 @@
 <?php
-namespace WP_CLI;
 
 use WP_CLI;
 use WP_CLI\Process;
 use WP_CLI\Utils;
 
-class Cf7Command {
+class Cf7Command  extends WP_CLI_Command {
 
 	public function create($args, $assoc_args) {
 		WP_CLI::success( "Teste" );
--- wp-cf7-command.php
+++ wp-cf7-command.php
@@ -1,9 +1,26 @@
 <?php
-if ( ! class_exists( 'WP_CLI' ) ) {
-	return;
-}
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/**
+* @package Resultate-Create-CF7-WP-CLI
+* Plugin Name: Resultate Create Contact Form 7 WP CLI
+* Description: Este plugin funciona como uma extensão para o WP-CLI para criar formulários do Contact Form 7 via linha de comando
+* Version: 0.0.1
+* Author: Resultate
+* Author URI: http://www.resultate.com.br
+*/
+
+use WP_CLI;
 $autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
 if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
-WP_CLI::add_command( 'cf7 create', array( 'WP_CLI\Cf7Command', 'create' ) );
+
+class Cf7Command  extends WP_CLI_Command {
+
+	public function create($args, $assoc_args) {
+		WP_CLI::success( "Teste" );
+	}
+}
+
+WP_CLI::add_command( 'test', 'Cf7Command' );
